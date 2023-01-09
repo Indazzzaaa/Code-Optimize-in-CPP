f6880c3dcc9ed6967d555569a4de8dbe610afe4e
Indzzzaaa <patelsumant19@gmail.com>
2023-01-09 06:30:58 +0530

    Starting oops

---
+++ testing/default.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    int a;
+    char c;
+    int *x = new int;
+    int *z = new int[10];
+
+    cout << a << endl;
+    cout << c << endl;
+    cout << *x << endl;
+    cout << z[2] << endl;
+
+    return 0;
+}
