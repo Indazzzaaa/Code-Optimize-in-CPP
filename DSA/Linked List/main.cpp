616a20aadbed630a2e869b1c849a01b7bd7cb45d
Indzzzaaa <patelsumant19@gmail.com>
2023-02-07 09:49:29 +0530

    completed the singlyLL

--- DSA/Linked List/main.cpp
+++ DSA/Linked List/main.cpp
@@ -1,1 +1,75 @@
 #include <iostream>
+#include "singly.cpp"
+using namespace std;
+
+void testLoopDetection()
+{
+    singlyLL *head = NULL, *end = NULL;
+    for (int i = 0; i < 10; i++)
+    {
+        head = head->insertEnd(head, i);
+    }
+    singlyLL::print(head);
+    end = head->findNthFromEnd(head, 1);
+    end->next = head->next->next->next; // !loop on node with value =3
+    singlyLL::detectLoop(head);
+}
+
+void testLLIntersection()
+{
+
+    singlyLL *h1 = NULL, *h2 = NULL, *temp = NULL;
+    for (int i = 3; i < 6; i++)
+    {
+        h1 = h1->insertEnd(h1, (i - 3) * 10);
+        h2 = h2->insertEnd(h2, (i - 4) * 100);
+        temp = temp->insertEnd(temp, i);
+    }
+    (h1->findNthFromEnd(h1, 1))->next = temp;
+    (h2->findNthFromEnd(h2, 1))->next = temp;
+
+    singlyLL::print(temp);
+    singlyLL::print(h1);
+    singlyLL::print(h2);
+    singlyLL::interSectionPoint(h1, h2);
+}
+
+void testMergeLL()
+{
+    singlyLL *h1 = NULL, *temp = NULL;
+    for (int i = 3; i < 6; i++)
+    {
+        h1 = h1->insertEnd(h1, (i - 3) * 10);
+        temp = temp->insertEnd(temp, i);
+    }
+
+    singlyLL::print(temp);
+    singlyLL::print(h1);
+    singlyLL::print(singlyLL::mergeLL(h1, temp));
+}
+
+void testSorting()
+{
+    singlyLL *head = NULL;
+    for (int i = 0; i < 1000; i++)
+    {
+        int num = rand() % 100;
+        head = head->insertEnd(head, num);
+    }
+    cout << "Before Sorting : " << endl;
+    singlyLL::print(head);
+    head = singlyLL::sort(head);
+    cout << "After sorting : " << endl;
+    singlyLL::print(head);
+}
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    // testLoopDetection();
+    // testLLIntersection();
+    // testMergeLL();
+    testSorting();
+    cin.get();
+    return 0;
+}
--- DSA/Linked List/singly.cpp
+++ DSA/Linked List/singly.cpp
@@ -73,7 +73,7 @@ struct singlyLL
         singlyLL *curr = head;
         // ! just for the case position does not exist
         singlyLL *followPtr = NULL;
-        for (int i = 1; i < pos - 1 && curr != NULL, i++)
+        for (int i = 1; i < pos - 1 && curr != NULL; i++)
         {
             followPtr = curr;
             curr = curr->next;
@@ -110,4 +110,260 @@ struct singlyLL
 
         return false;
     }
-}
+
+    // * Prints the LL in required way
+    static void print(singlyLL *head)
+    {
+        if (head == NULL)
+        {
+            cout << "Our List is Empty  " << endl;
+            return;
+        }
+        while (head != NULL)
+        {
+            cout << "[ " << head->data << " ]";
+            cout << "---->";
+            head = head->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    singlyLL *reverse(singlyLL *&head)
+    {
+        if (head == NULL)
+            cout << "Nothing to reverse LL is Empty! " << endl;
+        singlyLL *prev = NULL;
+        singlyLL *curr = head;
+        while (curr != NULL)
+        {
+            singlyLL *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    // if even nodes then it will return node n/2th
+    singlyLL *middleNode(singlyLL *&head)
+    {
+        if (head == NULL)
+            return NULL;
+
+        singlyLL *slow = head, *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    singlyLL *findNthFromEnd(singlyLL *&head, int n)
+    {
+        if (head == NULL)
+            return NULL;
+        singlyLL *backPtr = head, *frwdPtr = head;
+        for (int i = 0; i < n && frwdPtr != NULL; i++)
+        {
+            frwdPtr = frwdPtr->next;
+        }
+
+        if (frwdPtr != NULL)
+        {
+            while (frwdPtr != NULL)
+            {
+                backPtr = backPtr->next;
+                frwdPtr = frwdPtr->next;
+            }
+        }
+
+        return backPtr;
+    }
+
+    singlyLL *reverseInGroups(singlyLL *head, int k)
+    {
+
+        bool isFirstPass = true;
+        singlyLL *curr = head, *prevPtr = NULL;
+        while (curr != NULL)
+        {
+            singlyLL *prev = NULL;
+            singlyLL *currStart = curr;
+            for (int i = 0; i < k && curr != NULL; i++)
+            {
+                singlyLL *next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+            if (isFirstPass)
+            {
+
+                head = prev;
+                isFirstPass = false;
+            }
+            else
+            {
+                prevPtr->next = prev;
+            }
+            prevPtr = currStart;
+        }
+
+        return head;
+    }
+    // !I have designed this algorithm assuming that loop does exist
+    static void detectLoop(singlyLL *head)
+    {
+        if (head == NULL)
+            cout << "No loop exist" << endl;
+
+        singlyLL *slow = head, *fast = head;
+        do
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        } while (slow != fast);
+
+        slow = head;
+        while (slow->next != fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        cout << "Loop found at node : " << slow->next->data << endl;
+
+        // couting the nodes in loop
+        slow = slow->next;
+        fast = slow;
+        int count = 0;
+        do
+        {
+            fast = fast->next;
+            count++;
+        } while (fast != slow);
+
+        cout << "Number of nodes inside the loop are : " << count << endl;
+    }
+
+    static void interSectionPoint(singlyLL *head1, singlyLL *head2)
+    {
+        if (head1 == NULL || head2 == NULL)
+            cout << "No intersection point lies " << endl;
+
+        // counting length of head1 and head2 nodes
+        int h1count = 0, h2count = 0;
+        singlyLL *curr1 = head1, *curr2 = head2;
+        while (curr1 != NULL)
+        {
+            h1count++;
+            curr1 = curr1->next;
+        }
+        while (curr2 != NULL)
+        {
+            h2count++;
+            curr2 = curr2->next;
+        }
+
+        cout << "Node count in LL1,LL2 is : ( " << h1count << ", " << h2count << ") " << endl;
+        // we are using head1 for longer list and head2 for smaller
+        if (h1count < h2count)
+        {
+            swap(head1, head2);
+            swap(h1count, h2count);
+        }
+
+        // increamenting the LL of head1 by h1count-h2Count
+        for (int i = 0; i < h1count - h2count; i++)
+        {
+            head1 = head1->next;
+        }
+
+        while (head1 != head2)
+        {
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+
+        cout << "Intersection found at : " << head1->data << endl;
+    }
+
+    static singlyLL *sort(singlyLL *head)
+    {
+        // !checking the partition level
+        // static int partition = 0;
+        // partition++;
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        // finding the mid of the link
+        singlyLL *slow = head, *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        // we are breaking link after slow and creating the two links
+        auto temp = slow->next;
+        slow->next = NULL;
+        slow = temp;
+        /* cout << "Partition : " << partition << endl;
+        print(head);
+        print(slow); */
+        singlyLL *head1 = sort(head);
+        singlyLL *head2 = sort(slow);
+        return mergeLL(head1, head2);
+    }
+
+    static singlyLL *mergeLL(singlyLL *head1, singlyLL *head2)
+    {
+        if (head1 == NULL)
+            return head2;
+        if (head2 == NULL)
+            return head1;
+
+        singlyLL *resultHead = NULL, *resultEnd = NULL;
+        while (head1 != NULL && head2 != NULL)
+        {
+            singlyLL *temp = NULL;
+            if (head1->data > head2->data)
+            {
+                temp = head2;
+                head2 = head2->next;
+            }
+            else
+            {
+
+                temp = head1;
+                head1 = head1->next;
+            }
+
+            temp->next = NULL;
+            if (resultHead == NULL)
+            {
+                resultHead = temp;
+                resultEnd = temp;
+            }
+            else
+            {
+                resultEnd->next = temp;
+                resultEnd = resultEnd->next;
+            }
+        }
+
+        if (head1 == NULL)
+        {
+            resultEnd->next = head2;
+        }
+        else
+        {
+            resultEnd->next = head1;
+        }
+
+        return resultHead;
+    }
+};
